1e5287d5e3c7691a64a2731cebc12103dc943d54
yanghyunyangha <bw03306@gmail.com>
2023-03-24 16:55:29 +0900

    hosting

--- src/components/header/Logo.svelte
+++ src/components/header/Logo.svelte
@@ -1,7 +1,12 @@
 <script>
+    import logo from "@images/favicon.svg"
+
     export let styles;
 </script>
 
 <h1 class="{ styles.logo } semi">
-    <a href="/worklist/">w<span class="theme-bg"></span></a>
+    <a href="/worklist/">
+        <img src="{ logo }" alt="">
+        <!-- <span class="theme-bg"></span> -->
+    </a>
 </h1>
